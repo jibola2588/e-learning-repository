e30d57ad12e26ddc7a8b84e5211760875813254a
Fredrickinyang <fredrickinyang@gmail.com>
2022-08-17 16:05:54 +0100

    created courses page and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
-import {LandingPage} from './pages/home/Landing-Page'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { LandingPage } from "./pages/home/Landing-Page";
+import CoursePage from "./pages/coursePage/CoursePage";
 
 const App = () => {
   return (
-   <Router>
+    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/courses" element={<CoursePage />} />
       </Routes>
-   </Router>
+    </Router>
   );
-}
+};
 
 export default App;
