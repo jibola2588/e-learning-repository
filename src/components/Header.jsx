7724c87085da5608c5844f9c1d4483010dde7ea3
jibola2588 <jibolafawole258@gmail.com>
2022-08-17 12:44:41 -0700

    added landing page

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+          <div className="">
+              
+             
+           </div>
+    
+     </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/home/Landing-Page.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../components/navbars/Landing-Page-Navbar"
+import Header from "../../components/Header"
+
+export const LandingPage = () => { 
+    return ( 
+        <div>
+            <Navbar />
+            <Header />
+        </div>
+    )
+}
