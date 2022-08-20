2dab7816cae74a5d287a6a752f5220a4e3b90758
Ellovick1 <ezekielelom1@gmail.com>
2022-08-20 10:30:23 +0100

    added Elearning video to blog page

--- src/pages/blogPage/BlogPage.jsx
+++ src/pages/blogPage/BlogPage.jsx
@@ -1,23 +1,17 @@
 import Navbar from "../../components/navbars/Landing-Page-Navbar"
 import { HeroSection } from "../../components/blog-page/heroSection"
-import ELearningVideo  from "../../components/blog-page/E-learningVideo"
-import LearnFromSection  from "../../components/blog-page/Learn-from-Section"
 import { Tabs } from "../../components/blog-page/Tabs"
+import LearnFromAndVideo from "../../components/blog-page/LearnFromAndVideo"
 import Footer from "../../components/footer/Footer"
 
 export const BlogPage = () => {
     return (
         <div>
+            
             <Navbar />
             <HeroSection />
             <Tabs />
-            
-            <div className="grid lg:grid-cols-2 bg-[#5AB1BB] py-20">
-            < LearnFromSection />
-            < ELearningVideo />
-
-            </div>
-
+            <LearnFromAndVideo />
             <Footer />
 
         </div>
