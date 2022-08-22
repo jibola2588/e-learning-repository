0efd389b5bc64aaedd3d1cbc9236bc59bb877b5a
Teewai57 <bobtoyoabasi@gmail.com>
2022-08-22 13:59:47 +0100

    fixed leading universities

---
+++ src/components/landing-page/learn-from-section.jsx
@@ -0,0 +1,42 @@
+import DottedFrame from '../../assets/images/Dotted-frame.svg'
+
+export default function StartLearning() {
+    return (
+        <div className="left flex-2 pl-[7rem] ">
+                            <div>
+                                <h5 className="font-bold md:leading-[30px] text-[25px] md:text-[30px] lg:leading-[40px] lg:text-[35px] text-black">Learning from leading</h5>
+                                <h5 className="font-bold md:leading-[30px] text-[25px] md:text-[30px] lg:leading-[40px] lg:text-[35px]  text-black">Universities and companies</h5>
+                                <div className="mt-2 lg:mt-[14px]">
+                                    <p className="font-normal text-base leading-6 text-[#636468]">Start Streams on video demand lectures today from top</p>
+                                    <p className="font-normal text-base leading-6 text-[#636468]">instructors in subjects like business, computer science,</p>
+                                    <p className="font-normal text-base leading-6 text-[#636468]">data science, language learning & more.</p>
+                                </div>
+                                <div className="py-[16px]">
+                                <button 
+                                    className="font-bold flex-2 px-[32px] py-[14px] bg-[#00349A] rounded-full text-white text-sm leading-5 cursor-pointer hover:bg-[#fff] hover:text-[#00349A] border border-[#00349A]">
+                                    Start learning now
+                                </button>
+                                </div>
+                            </div>
+                        </div> 
+    )
+}
+
+
+{/* <section className=' lg:pl-36 md:px-32 px-10 lg:px-0'>
+    <div  className='grid mx-auto 
+     max-w-6xl 
+     items-center gap-y-16 gap-x-20 pr-4
+    lg:max-w-8x place-items-start'>
+            <div className="place-self-start relative">
+                <h5 className="text-[40px] font-[500] ">Learn from leading  universities and companies</h5>
+                <p className="col-5 text-[21px] md:pr-[6rem] mt-5 text-gray-500">
+                Start streams on video demand lectures today from top instructors in subject like business, computer science, data science, language learning, & more.
+                </p>
+                <button className="bg-[#00349A] rounded-[40px] text-white py-[18px] px-[32px] mt-7 relative z-10">Start Learning Now</button>
+                
+            </div>
+
+    </div>
+
+</section> */}
