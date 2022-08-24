66efa1149034c9fe568073610a1db99fde474e2d
Fredrickinyang <fredrickinyang@gmail.com>
2022-08-24 07:04:33 +0100

    fixed responsiveness for footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
-
     screens: {
       xsm: "400px",
       sm: "640px",
       md: "768px",
+      xmd: "830px",
       lg: "1024px",
       xl: "1280px",
       "2xl": "1536px",
